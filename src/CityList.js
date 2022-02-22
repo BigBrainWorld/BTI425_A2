62a0e4158f13aae1c4c489cffebc52deef294da3
Philippe Cormier <phili.C@hotmail.com>
2022-02-22 15:01:24 -0500

    Added ? notations

--- src/CityList.js
+++ src/CityList.js
@@ -41,7 +41,7 @@ export default function CityList(props){
             <ReactPaginate 
                 previousLabel={"<"} 
                 nextLabel={">"} 
-                pageCount={Math.ceil((props.cities ? props.cities.length / citiesPerPage : 0))}
+                pageCount={Math.ceil(props?.cities?.length / citiesPerPage)}
                 onPageChange={changePage}
                 containerClassName={"pageButtons"}
                 previousLinkClassName={"previousButton"}
